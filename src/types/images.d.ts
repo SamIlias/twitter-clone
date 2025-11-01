3a0d4b75ec5c56c2004ffcbaf7744e466628f2fb
SamIlias <Samovichilias19life@gmail.com>
2025-11-02 00:30:11 +0300

    chore: initial commit

---
+++ src/types/images.d.ts
@@ -0,0 +1,29 @@
+declare module '*.png' {
+  const value: { src: string; height: number; width: number; blurDataURL?: string };
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: { src: string; height: number; width: number; blurDataURL?: string };
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: { src: string; height: number; width: number; blurDataURL?: string };
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: { src: string; height: number; width: number; blurDataURL?: string };
+  export default value;
+}
+
+declare module '*.webp' {
+  const value: { src: string; height: number; width: number; blurDataURL?: string };
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default value;
+}
